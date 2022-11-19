5503050b4c85c6119c1b844224e637e25e85f0e2
Даниил Костюков <bicfine444@gmail.com.com>
2022-11-19 13:19:25 +0300

    save

---
+++ yatube/posts/models.py
@@ -0,0 +1,100 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from django import forms
+
+from .models import Post
+from core.models import CreatedModel
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Заголовок'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='ЧПУ'
+    )
+    description = models.TextField(
+        max_length=400,
+        verbose_name='Описание'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Группы'
+        verbose_name = 'Группу'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(CreatedModel):
+    text = models.TextField(
+        max_length=400,
+        help_text='Введите текст поста',
+        verbose_name='Текст поста'
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Группа',
+        help_text='Выберите группу'
+    )
+    image = models.ImageField(
+        'Картинка',
+        upload_to='posts/',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name_plural = 'Посты'
+        verbose_name = 'Пост'
+
+    def __str__(self):
+        return self.text[:15]
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    text = models.TextField(
+        'Текст',
+        help_text='Текст нового комментария',
+        blank=False
+    )
+    created = models.DateTimeField(
+        'Дата создания',
+        auto_now_add=True
+    )
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        # Добавили поле image в форму
+        fields = ('group', 'text', 'image')
